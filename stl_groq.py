f47357d65f2378009d5b8a0ae5c975e6149f8eaa
EdwFa <efart@mail.ru>
2024-05-21 17:11:34 +0300

    Update

--- stl_groq.py
+++ stl_groq.py
@@ -39,17 +39,15 @@ models = {
     "mixtral-8x7b-32768": {"name": "Mixtral-8x7b-Instruct-v0.1", "tokens": 32768, "developer": "Mistral"},
 }
 
-# Layout for model selection and max_tokens slider
-col1, col2 = st.columns(2)
-
-with col1:
-    model_option = st.selectbox(
-        "Choose a model:",
-        options=list(models.keys()),
-        format_func=lambda x: models[x]["name"],
-        index=4  # Default to mixtral
-    )
-
+# Add customization options to the sidebar
+st.sidebar.title('Настройки')
+system_prompt = st.sidebar.text_input("Промт:")
+model_option = st.sidebar.selectbox(
+    "Choose a model:",
+    options=list(models.keys()),
+    format_func=lambda x: models[x]["name"],
+    index=1  # Default to llama3-70B
+)
 # Detect model change and clear chat history if model has changed
 if st.session_state.selected_model != model_option:
     st.session_state.messages = []
@@ -57,17 +55,44 @@ if st.session_state.selected_model != model_option:
 
 max_tokens_range = models[model_option]["tokens"]
 
-with col2:
-    # Adjust max_tokens slider dynamically based on the selected model
-    max_tokens = st.slider(
-        "Max Tokens:",
-        min_value=512,  # Minimum value to allow some flexibility
-        max_value=max_tokens_range,
-        # Default value or max allowed if less
-        value=min(32768, max_tokens_range),
-        step=512,
-        help=f"Adjust the maximum number of tokens (words) for the model's response. Max for selected model: {max_tokens_range}"
-    )
+
+
+# Adjust max_tokens slider dynamically based on the selected model
+max_tokens = st.sidebar.slider(
+    "Max Tokens:",
+    min_value=512,  # Minimum value to allow some flexibility
+    max_value=max_tokens_range,
+    # Default value or max allowed if less
+    value=min(32768, max_tokens_range),
+    step=512,
+    help=f"Adjust the maximum number of tokens (words) for the model's response. Max for selected model: {max_tokens_range}"
+)
+
+
+# Layout for model selection and max_tokens slider
+# col1, col2 = st.columns(2)
+
+# with col1:
+#     model_option = st.selectbox(
+#         "Choose a model:",
+#         options=list(models.keys()),
+#         format_func=lambda x: models[x]["name"],
+#         index=2  # Default to llama3-70B
+#     )
+
+
+
+# with col2:
+#     # Adjust max_tokens slider dynamically based on the selected model
+#     max_tokens = st.slider(
+#         "Max Tokens:",
+#         min_value=512,  # Minimum value to allow some flexibility
+#         max_value=max_tokens_range,
+#         # Default value or max allowed if less
+#         value=min(32768, max_tokens_range),
+#         step=512,
+#         help=f"Adjust the maximum number of tokens (words) for the model's response. Max for selected model: {max_tokens_range}"
+#     )
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
