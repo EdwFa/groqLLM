ab00f9236a4fb9c7a55b8166421e47f5cba40686
EdwFa <efart@mail.ru>
2024-05-30 15:28:52 +0300

    Update

--- app.py
+++ app.py
@@ -75,7 +75,6 @@ def main():
 
     # Add customization options to the sidebar
     st.sidebar.title('Настройки')
-    # model = 'llama3-70b-8192'
     model = st.secrets["for_patient"]
 
     conversational_memory_length = st.sidebar.slider('Глубина памяти:', 1, 10, value=7)
